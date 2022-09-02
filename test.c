8322dc309481500937eee5050b56bfe59bdb1f9b
Is_TF <91179700@qq.com>
2022-09-02 11:47:13 +0800

    练习GitHub代码

---
+++ test.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	printf("tianfu\n\n\n\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("tianfu\n");
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//		return 0; 
+//}
+
+//int main()
+//{
+//	int age = 20;
+//	double weight = 75.3;
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum );
+
+	return 0;
+}
